75684410a08834e239b53f45a040bc83f5c551ff
Jhia Turner <jhimitu@gmail.com>
2019-01-31 13:49:54 -0700

    add portfolio and contact page, edit bookings page

---
+++ booking.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class Booking extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#booking_container');
+ReactDOM.render(e(Booking), domContainer);
